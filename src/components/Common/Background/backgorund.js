f59e2624a7b856ed82cc220c02ab75e16aec8f25
Matheus Oliveira <matheusops.pc@gmail.com>
2021-09-17 16:12:05 -0400

    Background Test
    Look at the link in background.js

--- src/components/Common/Background/backgorund.js
+++ src/components/Common/Background/backgorund.js
@@ -1,5 +1,8 @@
 import styled from 'styled-components'
 
+export const Container = styled.div`
+`;
+
 export const Box = styled.div`
 
 `;
@@ -8,3 +11,4 @@ export const Image = styled.img`
 
 `;
 
+////https://css-tricks.com/css-only-carousel/////
--- src/components/Common/Background/index.jsx
+++ src/components/Common/Background/index.jsx
@@ -1,19 +1,19 @@
 import './background.css'
-import { Box, Image } from './backgorund'
+import { Container, Box, Image } from './backgorund'
 import bg1 from '../../../styles/assets/bg1.jpg'
-// import bg2 from '../../../styles/assets/bg2.jpg'
-// import bg3 from '../../../styles/assets/bg3.jpg'
+import bg2 from '../../../styles/assets/bg2.jpg'
+import bg3 from '../../../styles/assets/bg3.jpg'
 
 export default function Background(){
   
- 
   return(
     <>
+    <Container className="slideshow">
     <Box className="background fade">
       <Image className="png" src={bg1}/>
-      {/* <Image className="png" src={bg2}/>
-      <Image className="png" src={bg3}/> */}
     </Box>
+    </Container>
+    
     </>
   )
 }
