7724a8e205a4ced7ba13187e8e42298ca32ce45f
Matheus Oliveira <matheusops.pc@gmail.com>
2021-09-17 16:39:06 -0400

    Back to basics

--- src/components/Common/Background/backgorund.js
+++ src/components/Common/Background/backgorund.js
@@ -11,4 +11,22 @@ export const Image = styled.img`
 
 `;
 
+export const Section = styled.section`
+
+`;
+
+export const Ol = styled.ol`
+
+`;
+
+export const Li = styled.li`
+
+`;
+
+export const Snaper = styled.div`
+
+`;
+
+
+
 ////https://css-tricks.com/css-only-carousel/////
--- src/components/Common/Background/index.jsx
+++ src/components/Common/Background/index.jsx
@@ -1,13 +1,14 @@
 import './background.css'
 import { Container, Box, Image } from './backgorund'
 import bg1 from '../../../styles/assets/bg1.jpg'
-import bg2 from '../../../styles/assets/bg2.jpg'
-import bg3 from '../../../styles/assets/bg3.jpg'
+// import bg2 from '../../../styles/assets/bg2.jpg'
+// import bg3 from '../../../styles/assets/bg3.jpg'
 
 export default function Background(){
   
   return(
     <>
+    
     <Container className="slideshow">
     <Box className="background fade">
       <Image className="png" src={bg1}/>
